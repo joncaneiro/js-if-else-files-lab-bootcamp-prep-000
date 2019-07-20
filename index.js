c9451abdd5096151c22688b136c5ec735b5128c5
Jon Caneiro <joncaneiro@gmail.com>
2019-07-20 18:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var height = "74"
 
 if (name === susan) {
   console.log("The name is correct")
+} else {
+  console.log("Expected name to be susan")
 }
+
 
 // Don't worry about this
 module.exports = { name, height
