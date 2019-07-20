fd102dbcbf574def8f8947fd4f5a259fff443c52
Jon Caneiro <joncaneiro@gmail.com>
2019-07-20 19:05:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var name = "Susan"
 var height = "74"
 
 
-if (name === "susan") {
+if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log("Expected name to be Susan")
